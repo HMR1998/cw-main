e08c4a818b2df05e987d93480ba686b1651cf0fe
Malik-Ramzan, Humza (PG/T - Computer Science) <hm00547@surrey.ac.uk>
2023-05-10 07:46:54 +0100

    working on results section

--- index.py
+++ index.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 import os
 import logging
+import random
 import boto3
 import time
 import http.client
 from concurrent.futures import ThreadPoolExecutor
+from data import data
 
-from flask import Flask, request, render_template
+from flask import Flask, redirect, request, render_template, session, url_for
 
 app = Flask(__name__)
+app.secret_key = 'password'
 
 def doRender(tname, values={}):
 	if not os.path.isfile( os.path.join(os.getcwd(), 'templates/'+tname) ): #No such file
-		return render_template('main.htm')
+		return render_template('form.htm')
 	return render_template(tname, **values) 
 
 @app.route('/')
 def index():
     return render_template('form.htm')
+
+
+@app.route('/results')
+def result_page():
+    
+    service = session.get('service')
+    resources = session.get('resources')
+    
+    return render_template('results.htm', service=service, resources=resources)
 
 @app.route('/submit', methods=['POST'])
 def initialisation():
@@ -27,15 +39,19 @@ def initialisation():
     
     if service == 'EC2':
         launch_ec2_instances(resources)
-        return render_template(message = f'{resources} EC2 instances launched. Elapsed Time: {time.time() - start}')
     elif service == 'Lambda':
         launch_lambda(resources)
-        return render_template(message = f'{resources} Lambda functions launched. Elapsed Time: {time.time() - start}')
     else:
-        return render_template(message = 'Invalid service selected.')
+        return render_template('form.htm', message='Invalid service selected.')
     
-    return print( "Elapsed Time: ", time.time() - start)
-
+    eta_time = time.time() - start
+    message = get_eta(eta_time, service)
+    
+    session['service'] = service
+    session['resources'] = resources
+    
+    return redirect(url_for('result_page'))
+        
 def launch_ec2_instances(num_instances):
     region = 'us-east-1'
     ec2 = boto3.client('ec2', region)
@@ -69,6 +85,45 @@ def launch_lambda(num_instances):
         results = executor.map(lambda_initialization, range(num_instances))
     return results
 
+
+def get_eta(eta_time, service):
+    if service == 'EC2':
+        message = f'{eta_time} seconds elapsed for EC2 instances.'
+    elif service == 'Lambda':
+        message = f'{eta_time} seconds elapsed for Lambda functions.'
+    else:
+        message = f'Invalid service selected.'
+    
+    return message
+
+@app.route('/results', methods=['POST'])
+def get_calculations():
+    service = session.get('service')
+    resources = session.get('resources')
+    min_history = int(request.form['min_history'])
+    shots = int(request.form['shots'])
+    results = []
+    means = []
+    stds = []
+    
+    for resource in range(resources):
+        for i in range(min_history, len(data)):
+            if data.Buy[i] == 1:
+                mean = data.Close[i - min_history:i].pct_change(1).mean()
+                std = data.Close[i - min_history:i].pct_change(1).std()
+                simulated = [random.gauss(mean, std) for _ in range(shots)]
+                simulated.sort(reverse=True)
+                var95 = simulated[int(len(simulated) * 0.95)]
+                var99 = simulated[int(len(simulated) * 0.99)]
+                results.append((var95, var99))
+                means.append(mean)
+                stds.append(std)
+        print(means)
+        print(stds)
+
+    return render_template('results.htm', results=results, means=means, stds=stds)
+
+    
 
 # catch all other page requests - doRender checks if a page is available (shows it) or not (index)
 @app.route('/', defaults={'path': ''})
